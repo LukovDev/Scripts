c54ee1a4ecfe67a60aca7ec5c3e87d4dcc379426
Lukov <103067811+LukovDev@users.noreply.github.com>
2024-07-24 02:23:15 +0300

    commit 24 july 2024

--- cliker.py
+++ cliker.py
@@ -11,25 +11,23 @@ if True:
 
 
 # Параметры:
-button = 0    # 0 = LEFT | 1 = MIDDLE | 2 = RIGHT.
-cps    = 100  # Кликов в секунду.
-hotkey = "`"  # Горячая клавиша на клавиатуре.
-exit_k = "-"  # Выход из автокликера.
+button = 0        # 0 = LEFT | 1 = MIDDLE | 2 = RIGHT.
+cps    = 100      # Кликов в секунду.
+hotkey = "num -"  # Горячая клавиша на клавиатуре.
+exit_k = "num *"  # Выход из автокликера.
 
 
 # Внутренние переменные:
 is_ext = False  # Выйти ли из автокликера.
 work   = False  # Включен ли автокликер.
 
-
 # Делаем бинды на клавиши клавиатуры:
-if True:
-    keyboard.add_hotkey(hotkey.lower(), lambda: globals().update(work=not globals()["work"]))      # Бинд на активацию.
-    keyboard.add_hotkey(exit_k.lower(), lambda: globals().update(is_ext=not globals()["is_ext"]))  # Бинд на выход.
+keyboard.add_hotkey(hotkey.lower(), lambda: globals().update(work=not globals()["work"]))      # Бинд на активацию.
+keyboard.add_hotkey(exit_k.lower(), lambda: globals().update(is_ext=not globals()["is_ext"]))  # Бинд на выход.
 
 
 # Бесконечно быстро обрабатываем клики:
 while not is_ext:
     if work:
         mouse.click(button=["left", "middle", "right"][button])
-        time.sleep(1/cps)
+    time.sleep(1/cps)
